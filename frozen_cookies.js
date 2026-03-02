476cfd5d0ac613238be25bb50bcebfdc87c0d973
sokusekimen-ship-it <sokusekimen@gmail.com>
2026-03-02 19:40:43 +0900

    Update frozen_cookies.js

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -20,7 +20,7 @@ var scriptElement =
 var baseUrl =
     scriptElement !== null
         ? scriptElement.getAttribute("src").replace(/\/frozen_cookies\.js$/, "")
-        : "https://github.erbkaiser.com/FrozenCookies/";
+        : "https://sokusekimen-ship-it.github.io/FrozenCookies/";
 var FrozenCookies = {
     baseUrl: baseUrl,
     branch: "erb-",
